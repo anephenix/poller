e6094faf15b386e2609e588be10519e6800cbb7b
Paul Jensen <paulbjensen@gmail.com>
2014-07-03 19:34:20 +0100

    Another test for this

--- index.js
+++ index.js
@@ -31,11 +31,12 @@ function poller (folderPath, cb) {
 
 		fs.stat(folderPath, function (err, stats) {
 
-			console.log('err is');
-			console.log(err);
-			console.log('/err is');
-
-			if (err) return cb(err);
+			if (err) {
+				console.log('err occurred');
+				console.log(err);
+				console.log('/err');
+				return cb(err);
+			}
 			if (!stats.isDirectory()) return cb(new Error('The path you passed is not a folder: ' + folderPath));
 
 			// Now you can start polling this
