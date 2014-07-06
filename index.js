8cfbaec8c47d671940f21660b3039ec198ac99bc
Paul Jensen <paulbjensen@gmail.com>
2014-07-06 14:08:15 +0100

    Added comments to the code, so that other people could pick the library and understand it

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ function poller (folderPath, optionsOrCb, cb) {
 
 	if (!folderPath) {
 
+		// return an error explaining that the user needs to pass a folder path as the 1st argument
+		//
 		var argumentType = typeof folderPath;
 		return cb(new Error('You need to pass a folder path, you passed an argument with type: ' + argumentType));
 
@@ -31,49 +33,72 @@ function poller (folderPath, optionsOrCb, cb) {
 
 	fs.exists(folderPath, function (exists) {
 
-		if (!exists) {
-			return cb(new Error('This folder does not exist: ' + folderPath));
-		}
+		// return an error if the folder does not exist
+		//
+		if (!exists) return cb(new Error('This folder does not exist: ' + folderPath));
 
 		fs.stat(folderPath, function (err, stats) {
 
 			if (err) return cb(err);
+
+			// return an error if the path is not a folder
+			//
 			if (!stats.isDirectory()) return cb(new Error('The path you passed is not a folder: ' + folderPath));
 
-			// Now you can start polling this
+			// Generate a polling event emitter
+			//
 			var poll = new eventEmitter();
 
+
+			// Get the initial list of files in the folder 
+			//
 			fs.readdir(folderPath, function (err, files) {
 
 				poll.files = files;
 
+
+				// Setup the internal watch function
+				//
 				poll.watch = function () {
 
 					var interval;
 
+					// If an interval option was passed, use that number as the interval,
+					// otherwise default to 100ms
+					//
 					if (typeof optionsOrCb === 'object' && typeof optionsOrCb.interval === 'number') {
 						interval = optionsOrCb.interval;
 					} else {
 						interval = 100;
 					}
 
+					// Setup the interval function
+					//
 					poll.timeout = setInterval(function () {
 
 						fs.readdir(folderPath, function (err, files) {
 
 							if (err) throw err;
 
+							// Get the list of files that have been added or removed
+							// 
 							var addedFiles		= _.difference(files, poll.files);
 							var removedFiles	= _.difference(poll.files, files);
 
 							if (addedFiles.length > 0) {
+								// Emit an add event with the full path of the added file
+								//
 								addedFiles.forEach(function (addedFile) { poll.emit('add', folderPath + '/' + addedFile); });
 							}
 
 							if (removedFiles.length > 0) {
+								// Emit a remove event with the full path of the removed file
+								//
 								removedFiles.forEach(function (removedFile) { poll.emit('remove', folderPath + '/' + removedFile); });
 							}
 
+							// Set the list of tracked files to the files that were polled
+							//
 							poll.files = files;
 
 						});
@@ -82,21 +107,24 @@ function poller (folderPath, optionsOrCb, cb) {
 				};
 
 
-
+				// Sets a function to allow the user to stop the polling
+				//
 				poll.close = function () {
 					clearInterval(poll.timeout);
 				};
 
 
-
+				// Calls the internal watch function to get the polling started
+				//
 				poll.watch();
 
+				// returns the error and poll variables to the user
+				//
 				cb(err, poll);
 
 			});
 
 		});
-
 
 	});
 
